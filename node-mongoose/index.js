46cf0ef6ba8b52107618c44b99300dac1022f9f0
Sunil Shah <sunilshah2416@gmail.com>
2021-06-16 08:10:28 +0545

    Node Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -7,24 +7,22 @@ const connect = mongoose.connect(url,{ useNewUrlParser: true });
 connect.then((db)=>{
     console.log("Connected succesfully to the database");
 
-    var newDish = Dishes({
+    Dishes.create({
         name:'Uthappizza',
         description:'test'
-    });
-
-    newDish.save()
-        .then((dish)=>{
-            console.log(dish);
-            return Dishes.find({});
-        })
-        .then((dishes)=>{
-            console.log(dishes);
-            return Dishes.deleteMany({});
-        })
-        .then(()=>{
-            return mongoose.connection.close();
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
+    })
+    .then((dish)=>{
+        console.log(dish);
+        return Dishes.find({});
+    })
+    .then((dishes)=>{
+        console.log(dishes);
+        return Dishes.deleteMany({});
+    })
+    .then(()=>{
+        return mongoose.connection.close();
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
 })
