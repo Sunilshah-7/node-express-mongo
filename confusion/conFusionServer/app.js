7d810808f899d0809c165969633c7a043a35905a
Sunil Shah <sunilshah2416@gmail.com>
2021-06-18 23:46:32 +0545

    Express Session Part 2

--- confusion/conFusionServer/app.js
+++ confusion/conFusionServer/app.js
@@ -32,7 +32,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
   name:'session-id',
@@ -41,47 +40,29 @@ app.use(session({
   resave:false,
   store: new FileStore()
 }))
-app.use(auth);
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 function auth(req,res,next){
   if(!req.session.user){
-    console.log(req.session);
-    var authHeader = req.headers.authorization;
-    if(!authHeader){
-      err = new Error('User is not authorized');
-      res.setHeader('WWW-Authenticate','Basic');
-      err.status = 401;
-      next(err);
-      return;
-    }
-    var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if(user=='admin'&&pass=='password'){
-      req.session.user = 'admin';
-      next(); //authorised
-    }
-    else{
-      err = new Error('User is not authorized');
-      res.setHeader('WWW-Authenticate','Basic');
-      err.status = 401;
-      next(err);
-    }
+    var err = new Error('You are not authorized');
+    err.status=403;
+    next(err);
   }
   else{
-    if(req.session.user==='admin'){
-      console.log('Request session',req.session)
+    if(req.session.user==='authenticated'){
+      next();
     }
     else{
-      err = new Error('User is not authorized');
-      err.status = 401;
+      var err = new Error('You are not authorized');
+      err.status=403;
       next(err);
     }
   }
 }
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(auth);
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
