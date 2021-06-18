d8638ae85e22da59fe4eb4d842d8efb006084e88
Sunil Shah <sunilshah2416@gmail.com>
2021-06-18 09:14:33 +0545

    Cookie

--- confusion/conFusionServer/app.js
+++ confusion/conFusionServer/app.js
@@ -22,40 +22,52 @@ connect.then((db)=>{
   console.log("Connected to database successfully.");
 },(err)=>console.log(err));
 
-function auth(req,res,next){
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if(!authHeader){
-    err = new Error('User is not authorized');
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    next(err);
-    return;
-  }
-  var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
-  if(user==='admin'&&pass==='password'){
-    next(); //authorised
-  }
-  else{
-    err = new Error('User is not authorized');
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    next(err);
-  }
-}
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(auth);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(auth);
 
+function auth(req,res,next){
+  if(!req.signedCookies.user){
+    var authHeader = req.headers.authorization;
+    if(!authHeader){
+      err = new Error('User is not authorized');
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status = 401;
+      next(err);
+      return;
+    }
+    var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if(user=='admin'&&pass=='password'){
+      res.cookie('user','admin',{signed:true})
+      next(); //authorised
+    }
+    else{
+      err = new Error('User is not authorized');
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status = 401;
+      next(err);
+    }
+  }
+  else{
+    if(req.signedCookies.user==='admin'){
+      next();
+    }
+    else{
+      err = new Error('User is not authorized');
+      err.status = 401;
+      next(err);
+    }
+  }
+}
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
