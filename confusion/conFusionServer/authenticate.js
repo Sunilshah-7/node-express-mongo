840ff2d2a85245b41112da42a2c8f33f65743dd4
Sunil Shah <sunilshah2416@gmail.com>
2021-07-10 13:04:22 +0545

    Passport Facebook

--- confusion/conFusionServer/authenticate.js
+++ confusion/conFusionServer/authenticate.js
@@ -3,6 +3,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var User = require('./models/users');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
+var FacebookTokenStrategy = require('passport-facebook-token');
 var jwt = require('jsonwebtoken');
 var config = require('./config');
 
@@ -10,36 +11,63 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken = function(user){
-    return jwt.sign(user,config.secretKey,{expiresIn:3600});
+exports.getToken = function (user) {
+    return jwt.sign(user, config.secretKey, { expiresIn: 3600 });
 }
 
-var opts={};
+var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
-exports.jwtPassport = passport.use(new JwtStrategy(opts,(jwt_payload,done)=>{
-    console.log("Jwt Payload:",jwt_payload);
-    User.findOne({_id:jwt_payload._id},(err,user)=>{
-        if(err){
-            return done(err,false);
-        }else if(user){
-            return done(null,user);
-        }else{
-            return done(null,false);
+exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    console.log("Jwt Payload:", jwt_payload);
+    User.findOne({ _id: jwt_payload._id }, (err, user) => {
+        if (err) {
+            return done(err, false);
+        } else if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
         }
     })
 }))
 
-exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyUser = passport.authenticate('jwt', { session: false });
 
-exports.verifyAdmin = function(req,res,next){
-    if(req.user.admin){
+exports.verifyAdmin = function (req, res, next) {
+    if (req.user.admin) {
         next();
     }
-    else{
+    else {
         var err = new Error("You are not authorized to perform this operation.");
-        err.status=403;
+        err.status = 403;
         return next(err);
     }
 };
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientID,
+    clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ facebookId: profile.id }, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (!err && user !== null) {
+            return done(null, user);
+        }
+        else {
+            user = new User({ username: profile.displayName });
+            user.facebookId = profile.id;
+            user.firstName = profile.name.givenName;
+            user.lastName = profile.name.familyName;
+            user.save((err, user) => {
+                if (err)
+                    return done(err, false);
+                else
+                    return done(null, user);
+            })
+        }
+    });
+}
+));
--- confusion/conFusionServer/config.js
+++ confusion/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports ={
     'secretKey':'12345-67890-09876-54321',
-    'mongoUrl':'mongodb://localhost:27017/conFusion'
+    'mongoUrl':'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        clientID:'333355271804953',
+        clientSecret:'022e6a7a8a2c868d8368c3818821a490'
+    }
 }
